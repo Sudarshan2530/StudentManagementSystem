113d50e53042f2cf8820eca34efe6753419430c7
Sudarshan2530 <garry159@gmail.com>
2024-03-29 18:55:12 +0530

    registration ui

--- frontend/src/app/update/update.component.ts
+++ frontend/src/app/update/update.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { error } from 'console';
+import { response } from 'express';
 
 @Component({
   selector: 'app-update',
@@ -6,5 +9,37 @@ import { Component } from '@angular/core';
   styleUrl: './update.component.css'
 })
 export class UpdateComponent {
+  // onSubmit(data:any){
+  //   console.log(data);
+  // }
+  name:any;
+  email:any;
+  password:any;
 
+
+  constructor(private http: HttpClient)
+  {
+
+  }
+
+
+  registerUser()
+  {
+    const userData = 
+    {
+      name: this.name,
+      email: this.email,
+      password:this.password,
+    }
+
+    this.http.post('http://localhost:4201/update', userData.email)
+    .subscribe((response:any)=>
+    {
+      console.log("You have registered successfully!!", response)
+    },
+    error=> {
+      console.log("Please fill details ", error);
+  
+    })
+  }
 }
